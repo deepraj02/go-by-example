400375ff85f5fbef9a43f5430e10b9c05dabec50
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-06-13 23:37:27 +0530

    🔥 (feat): slices in go

--- learning/arrays-slices/main.go
+++ learning/arrays-slices/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
 	theArrays()
-	
+	theSlices()
+
 }
 
 func theArrays() {
@@ -17,7 +19,6 @@ func theArrays() {
 	myArr[0] = "Hello"
 	fmt.Println(myArr[0])
 
-	
 	var a [5]int
 	fmt.Println("emp:", a)
 
@@ -44,4 +45,37 @@ func theArrays() {
 	}
 
 	fmt.Println("2d: ", twoD)
+}
+
+func theSlices() {
+	fmt.Println("-----------------\n\nSlices\n\n-----------------")
+
+	///Unlike arrays, slices are typed only by the elements they contain (not the number of elements). An uninitialized slice equals to nil and has length 0.
+
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	s = make([]string, 4)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	s[3] = "d"
+	fmt.Println("set:", s, "get:", s[2])
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+	for i, v := range s {
+		fmt.Println("idx:", i, "val:", v)
+	}
+	/// Slices can also be copy’d. Here we create an empty slice c of the same length as s and copy into c from s.
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	if slices.Equal(c, s) {
+		fmt.Println("c and s are equal")
+	}
 }
